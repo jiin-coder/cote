8950b6ad7eeed5d475b847ce2659d64688de07f2
kimjiae <fpahstiq123@naver.com>
2022-11-23 18:58:09 +0900

    기본세팅

---
+++ src/main/java/com/kja/rmcote/RmcoteApplication.java
@@ -0,0 +1,13 @@
+package com.kja.rmcote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmcoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmcoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kja/rmcote/RmcoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kja.rmcote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmcoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
