c71d898a641945e13d2558d6f26ef33164d1c910
kimjiae <fpahstiq123@naver.com>
2022-11-23 21:06:31 +0900

    Hello World 출력

--- src/main/java/com/kja/rmcote/RmcoteApplication.java
+++ src/main/java/com/kja/rmcote/RmcoteApplication.java
@@ -2,12 +2,21 @@ package com.kja.rmcote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RmcoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RmcoteApplication.class, args);
+	}
+
+	@GetMapping(value = "/")
+	public String HelloWorld(){
+		return "Hello World";
 	}
 
 }
