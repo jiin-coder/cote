2690ef61ef5a50a402c5971ff344ad37f323cc66
kimjiae <fpahstiq123@naver.com>
2022-11-24 23:06:46 +0900

    상품정보 entity 설계

--- src/main/java/com/kja/rmcote/domain/RmcoteApplication.java
+++ src/main/java/com/kja/rmcote/domain/RmcoteApplication.java
@@ -1,8 +1,7 @@
-package com.kja.rmcote;
+package com.kja.rmcote.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/java/com/kja/rmcote/domain/prod/ProductInfo.java
@@ -0,0 +1,43 @@
+package com.kja.rmcote.domain.prod;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@Entity
+public class ProductInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "prod_id")
+    private Long prodId; // 상품코드
+
+    @Column(name = "prod_name", nullable = false, length = 50)
+    private String prodName; //상품명
+
+    @Column(name= "price", nullable = false)
+    private int price; // 금액
+
+    @Column(name= "prod_reg_date", nullable = false)
+    private LocalDateTime prodRegDate; // 등록일자
+
+    @Column(name= "reg_co_name", nullable = false)
+    private String regCoName; // 등록업체명
+
+    @Builder
+    public ProductInfo(String prodName, int price, LocalDateTime prodRegDate, String regCoName){
+        this.prodName = prodName;
+        this.price = price;
+        this.prodRegDate = prodRegDate;
+        this.regCoName = regCoName;
+    }
+
+}
